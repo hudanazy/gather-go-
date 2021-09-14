accc19c3c519d1218fc38cdd574ba1779983fe54
n a <439200683@student.ksu.edu.sa>
2021-09-14 20:54:56 +0300

    Log in

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -10,6 +10,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _fromkey = GlobalKey<FormState>();
+
+
+  String username='';
+  String password='';
+  String error='';
 
   @override
   Widget build(BuildContext context) {
@@ -25,18 +31,48 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: ElevatedButton(
-          child: Text("Sign in anon"),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print("signing in error");
-            } else {
-              print("signed in successfully");
-              print(result);
-            }
-          },
-        ),
+        child: Form(
+          key: _fromkey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) => value!.isEmpty? 'Enter your username': null,
+                onChanged: (value) {
+                  setState(() => username=value);
+                },
+              ),
+              TextFormField(
+                obscureText: true,
+                validator: (value) => value!.isEmpty? 'Enter your passord': null,
+                onChanged: (value) {
+                  setState(() => password = value);
+                },
+              ),
+              ElevatedButton(
+                child: Text("Login"),
+                onPressed: () async {
+                // dynamic result = await _auth.signInAnon();
+                // if (result == null) {
+                // print("signing in error");
+                // } else {
+                //   print("signed in successfully");
+                //   print(result);
+                // }
+                if (_fromkey.currentState!.validate()){
+                  dynamic result = await _auth.SignInWithUsernameAndPassword(username, password);
+                  if (result == null)
+                    setState(() => error = 'Username or password is incorrect');
+                  }
+                },
+              ),
+              Text('Don’t have an account?'),
+              Text('Register now'),
+              SizedBox(height: 12.0,),
+              Text(
+                error,
+              )
+            ],)
+          )
       ),
     );
   }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,20 +1,46 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gather_go/Models/NewUser.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
 
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userInfoFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
     }
   }
+
+  //auth change stream
+  Stream<NewUser?> get user {
+    return _auth.authStateChanges()
+    .map((User? user) => _userInfoFromFirebaseUser(user));
+  }
+
+//getting a user from the firebase user
+  NewUser? _userInfoFromFirebaseUser(User? user){
+      if (user != null) return NewUser(uid: user.uid);
+      else return null;
+      
+  }
 //sign in anonymously method
 //sign in w/ email password
+Future SignInWithUsernameAndPassword(String username, String password) async{
+  try {
+    UserCredential result = await _auth.signInWithEmailAndPassword(email: username, password: password);
+    User? user = result.user;
+    return _userInfoFromFirebaseUser(user);
+  }
+  catch (e) {
+    print(e.toString());
+    return null;
+  }
+}
 //register
 //sign out
 
