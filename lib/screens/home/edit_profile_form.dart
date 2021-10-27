1d441075fd5e848987840340c783ce209e3fcdd9
hudanazy <90036532+hudanazy@users.noreply.github.com>
2021-10-27 08:38:03 +0300

    image

--- lib/screens/home/edit_profile_form.dart
+++ lib/screens/home/edit_profile_form.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:gather_go/Models/ProfileOnScreen.dart';
 import 'package:gather_go/Models/UesrInfo.dart';
@@ -9,8 +7,6 @@ import 'package:provider/provider.dart';
 import 'package:gather_go/Models/NewUser.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gather_go/shared/profile_widget.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:gather_go/shared/loading.dart';
 
 class epForm extends StatefulWidget {
   const epForm({Key? key}) : super(key: key);
@@ -37,173 +33,154 @@ class _epFormState extends State<epForm> {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController _controller =
-        TextEditingController(text: 'initial value');
-    UesrInfo? profileData;
-    final user = Provider.of<NewUser?>(context, listen: false);
-
-    Stream<QuerySnapshot<Map<String, dynamic>>> snap = FirebaseFirestore
-        .instance
-        .collection('uesrInfo')
-        .where('uid', isEqualTo: user?.uid)
-        .snapshots();
-    return StreamBuilder<Object>(
-        stream: snap, //DatabaseService(uid: user.uid).profileData,
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: Loading(),
-              //     child: Text(
-              //   "No New Events", // may be change it to loading , itis appear for a second every time
-              //   textAlign: TextAlign.center,
-              // )
-            );
+    final user = Provider.of<NewUser?>(context);
+    return StreamBuilder<UesrInfo>(
+        stream: DatabaseService(uid: user?.uid).profileData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UesrInfo profileData = snapshot.data as UesrInfo;
           }
-          return ListView(children: snapshot.data.docs.map<Widget>((document) {
-            DocumentSnapshot uid = document;
-            return Form(
-              key: _formkey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 50,
+          return Form(
+            key: _formkey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Edit your profile",
+                  style: TextStyle(
+                      color: Colors.orange[600],
+                      letterSpacing: 2,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: "Comfortaa"),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ProfileWidget(
+                  imagePath: "https://picsum.photos/200/300",
+//document['name'],
+                  isEdit: true,
+                  onClicked: () async {
+                    // _showProfilePanel();
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 320,
+                  child: TextFormField(
+                    //initialValue: profileData.name,
+                    decoration: textInputDecoration.copyWith(
+                      hintText: "What would like us to call you?",
+                      hintStyle: TextStyle(
+                          color: Colors.orange[600],
+                          fontSize: 14,
+                          fontFamily: "Comfortaa"),
+                    ),
+                    validator: (val) =>
+                        val!.isEmpty ? 'Please enter a name' : null,
+                    onChanged: (val) => setState(() => _currentName = val),
                   ),
-                  Text(
-                    "Edit your profile",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.only(top: 0, left: 5),
+                  child: Text(
+                    "Status",
                     style: TextStyle(
                         color: Colors.orange[600],
                         letterSpacing: 2,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
                         fontFamily: "Comfortaa"),
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  ProfileWidget(
-                    imagePath: "https://picsum.photos/200/300",
-                    //document['name'],
-                    isEdit: true,
-                    onClicked: () async {
-                      // _showProfilePanel();
-                    },
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    width: 320,
-                    child: TextFormField(
-                      //controller: _controller,
-                      initialValue: document['name'],
-                      decoration: textInputDecoration.copyWith(
-                        hintText: "What would like us to call you?",
-                        hintStyle: TextStyle(
-                            color: Colors.orange[600],
-                            fontSize: 14,
-                            fontFamily: "Comfortaa"),
-                      ),
-                      validator: (val) =>
-                          val!.isEmpty ? 'Please enter a name' : null,
-                      onChanged: (val) => setState(() => _currentName = val),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    alignment: Alignment.topLeft,
-                    padding: EdgeInsets.only(top: 0, left: 5),
-                    child: Text(
-                      "Status",
-                      style: TextStyle(
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                DropdownButtonFormField(
+                    value: _currentStatus ?? "Available",
+                    decoration: textInputDecoration,
+                    items: status.map((status) {
+                      return DropdownMenuItem(
+                        value: status,
+                        child: Text(status),
+                      );
+                    }).toList(),
+                    onChanged: (val) =>
+                        setState(() => _currentStatus = val as String),
+                    style: TextStyle(
+                      color: Colors.orange[600],
+                      fontFamily: 'Comfortaa',
+                    )),
+                SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 320,
+                  child: TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                      hintText: "Enter your bio.",
+                      hintStyle: TextStyle(
                           color: Colors.orange[600],
-                          letterSpacing: 2,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
                           fontFamily: "Comfortaa"),
                     ),
+                    maxLines: 4,
+                    validator: (val) =>
+                        val!.isEmpty ? 'Please enter a bio' : null,
+                    onChanged: (val) => setState(() => _currentBio = val),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  DropdownButtonFormField(
-                      value: _currentStatus ?? "Available",
-                      decoration: textInputDecoration,
-                      items: status.map((status) {
-                        return DropdownMenuItem(
-                          value: status,
-                          child: Text(status),
-                        );
-                      }).toList(),
-                      onChanged: (val) =>
-                          setState(() => _currentStatus = val as String),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 190,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[400]),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        padding: MaterialStateProperty.all(
+                            EdgeInsets.fromLTRB(35, 15, 35, 15))),
+                    child: Text(
+                      'Save Changes',
                       style: TextStyle(
-                        color: Colors.orange[600],
-                        fontFamily: 'Comfortaa',
-                      )),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    width: 320,
-                    child: TextFormField(
-                      decoration: textInputDecoration.copyWith(
-                        hintText: "Enter your bio.",
-                        hintStyle: TextStyle(
-                            color: Colors.orange[600],
-                            fontSize: 14,
-                            fontFamily: "Comfortaa"),
-                      ),
-                      maxLines: 4,
-                      validator: (val) =>
-                          val!.isEmpty ? 'Please enter a bio' : null,
-                      onChanged: (val) => setState(() => _currentBio = val),
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Comfortaa"),
                     ),
+                    onPressed: () async {
+                      if (_formkey.currentState!.validate()) {
+                        dynamic db = await DatabaseService(uid: user?.uid)
+                            .updateProfileData(
+                                user!.uid,
+                                _currentName!,
+                                _currentStatus!,
+                                _currentBio!,
+                                "https://picsum.photos/200/300");
+                        Navigator.pop(context);
+                        Fluttertoast.showToast(
+                          msg: "Profile successfully updated.",
+                          toastLength: Toast.LENGTH_LONG,
+                        );
+                      }
+                    },
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    width: 190,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.orange[400]),
-                          foregroundColor:
-                              MaterialStateProperty.all(Colors.white),
-                          padding: MaterialStateProperty.all(
-                              EdgeInsets.fromLTRB(35, 15, 35, 15))),
-                      child: Text(
-                        'Save Changes',
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                            fontFamily: "Comfortaa"),
-                      ),
-                      onPressed: () async {
-                        if (_formkey.currentState!.validate()) {
-                          dynamic db = await DatabaseService(uid: user?.uid)
-                              .updateProfileData(
-                                  user!.uid,
-                                  _currentName!,
-                                  _currentStatus!,
-                                  _currentBio!,
-                                  "https://picsum.photos/200/300");
-                          Navigator.pop(context);
-                          Fluttertoast.showToast(
-                            msg: "Profile successfully updated.",
-                            toastLength: Toast.LENGTH_LONG,
-                          );
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }));
+                ),
+              ],
+            ),
+          );
         });
   }
 
