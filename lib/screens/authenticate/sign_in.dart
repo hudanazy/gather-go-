0b3b516b99b223f750d75151646efd7c364e2cbe
shahadAD1 <73888466+shahadAD1@users.noreply.github.com>
2021-10-29 17:44:51 +0300

    fix import

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gather_go/services/auth.dart';
 import 'package:gather_go/shared/contants.dart';
 import 'package:gather_go/shared/loading.dart';
-import 'package:gather_go/screens/admin/eventDetails.dart';
+//import 'package:gather_go/screens/admin/eventDetails.dart';
 import 'package:gather_go/screens/authenticate/resetPassword.dart';
+import 'package:gather_go/screens/admin/eventdetailsLogo.dart';
 class SignIn extends StatefulWidget {
   // const SignIn({Key? key}) : super(key: key);
 
