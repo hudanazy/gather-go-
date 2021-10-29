24764157f162289cfde5438f911be5de13577d74
shahadAD1 <shahadabdullahd@gmail.com>
2021-10-29 03:01:07 +0300

    add new event details page for other user(with fixed route)

---
+++ lib/screens/home/eventDetailsForUsers.dart
@@ -0,0 +1,232 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:gather_go/screens/admin/adminEvent.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gather_go/screens/admin/eventdetailsLogo.dart';
+import 'package:gather_go/services/database.dart';
+import 'package:gather_go/shared/dialogs.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+// ignore: camel_case_types
+class eventDetailsForUesers extends StatefulWidget {
+  final DocumentSnapshot? event;
+  eventDetailsForUesers({required this.event});
+
+  @override
+  _eventDetails createState() => new _eventDetails();
+}
+
+// ignore: camel_case_types
+class _eventDetails extends State<eventDetailsForUesers> {
+  LocationData? currentLocation;
+  var location = new Location();
+  String error = "";
+
+  void initState() {
+    super.initState();
+
+    initPlatformState();
+
+    location.onLocationChanged.listen((LocationData result) {
+      setState(() {
+        currentLocation = result;
+      });
+    });
+  }
+
+  void initPlatformState() async {
+    LocationData? myLocation;
+    try {
+      myLocation = await location.getLocation();
+      error = "";
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED')
+        error = "permission denied";
+      else if (e.code == "PERMISSION_DENIED_NEVER_ASK")
+        error = "permission denied";
+      myLocation = null;
+    }
+
+    setState(() {
+      currentLocation = myLocation!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var curLat = currentLocation?.latitude ?? 0;
+    var curLong = currentLocation?.longitude ?? 0;
+    int attendeeNum = widget.event?.get('attendees');
+    String userID = widget.event?.get('uid');
+    String category = widget.event?.get('category');
+    List<Marker> myMarker = [];
+    eventCreator(userID);
+    LatLng markerPosition =
+        LatLng(widget.event?.get('lat'), widget.event?.get('long'));
+
+    setPolylines(markerPosition, curLong, curLat);
+    setState(() {
+      myMarker.add(Marker(
+        markerId: MarkerId(markerPosition.toString()),
+        infoWindow: InfoWindow(title: widget.event?.get('name')),
+        position: markerPosition, // markerPosition,
+        // draggable: true,
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: ArcBannerImage(),
+            ),
+            Row(children: [
+              IconButton(
+                icon: new Icon(Icons.arrow_back_ios),
+                onPressed: () {
+                  Navigator.pop(context,
+                      MaterialPageRoute(builder: (context) => adminEvent()));
+                },
+              ),
+              Flexible(
+                child: Text(widget.event?.get('name') + '   ',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontFamily: 'Comfortaa',
+                        fontSize: 18)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Chip(
+                  label: Text(category, style: TextStyle(color: Colors.black)),
+                  backgroundColor: Colors.grey[350],
+                ),
+              )
+            ]),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Edescription(widget.event?.get('description')),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.access_time),
+                  Text("   " +
+                      widget.event?.get('date').substring(0, 10) +
+                      "  " +
+                      widget.event?.get('time').substring(10, 15) +
+                      '                                                           '), // we may need to change it as i dont think this the right time !!
+                ],
+              ),
+            ),
+            //   Padding(padding: const EdgeInsets.only(left: 20.0),
+            //  child: Row(children: <Widget>[
+            //     Text("        ")])),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(children: <Widget>[
+                Icon(Icons.people_alt_rounded),
+                Text("   Max attendee number is $attendeeNum  ")
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, bottom: 20.0),
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.person_rounded,
+                ),
+                Text("   Created by   $_textFromFile")
+              ]),
+            ),
+
+            // decoration: new BoxDecoration(
+            //   color: Colors.black,
+            //   shape: BoxShape.circle,
+            //   border: Border.all(width: 5.0, color: Colors.white),
+            // ),
+            Padding(
+                padding: const EdgeInsets.only(right: 20.0, bottom: 20.0),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: Icon(
+                        Icons.location_pin,
+                        color: Colors.black,
+                      ),
+                      label: Text("see the location",
+                          style: TextStyle(
+                            color: Colors.black87,
+                          )),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                      ),
+                      //color: Colors.deepOrange,
+                      onPressed: () {
+                        showMapdialog(context, myMarker, _polylines);
+                      },
+                      //child: Text("see the location"),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Location _location = Location();
+  // late GoogleMapController _controller;
+  // void _onMapCreated(GoogleMapController _cntlr) {
+  //   _controller = _cntlr;
+  // }
+
+  String _textFromFile = "";
+  // will return eventCreator name
+  void eventCreator(String uid) async {
+    String uesrName = " ";
+    DocumentSnapshot documentList;
+    documentList =
+        await FirebaseFirestore.instance.collection('uesrInfo').doc(uid).get();
+
+    uesrName = documentList['name'];
+
+    setState(() => _textFromFile = uesrName);
+  }
+}
+
+Set<Polyline> _polylines = Set<Polyline>();
+List<LatLng> polylineCoordinates = [];
+PolylinePoints polylinePoints = new PolylinePoints();
+
+void setPolylines(LatLng a, double curLat, double curLong) async {
+  //LatLng cur = LatLng(curLat, curLong);
+  //getDirection(cur, a);
+  PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+    "AIzaSyDLPzEuq9j9sCXxxfr-U7LZMulEVeIbKKg",
+    PointLatLng(a.latitude, a.longitude),
+    PointLatLng(curLong, curLat),
+    travelMode: TravelMode.driving,
+  );
+  polylineCoordinates.clear();
+
+  if (result.status == 'OK') {
+    result.points.forEach((PointLatLng point) {
+      polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+    });
+
+    _polylines.add(Polyline(
+        width: 5,
+        polylineId: PolylineId('polyLine'),
+        color: Colors.orangeAccent,
+        points: polylineCoordinates));
+  }
+}
