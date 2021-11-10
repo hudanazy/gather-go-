0ae4136bb6ea3d579de473c6318e7c282c7b2922
hudanazy <90036532+hudanazy@users.noreply.github.com>
2021-11-10 18:36:29 +0300

    beginning of add comment

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -1,8 +1,14 @@
 //import 'dart:html';
 
-import 'package:flutter/material.dart';
+//import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:gather_go/Models/NewUser.dart';
+
 import 'package:gather_go/screens/home/eventDetailsForUsers.dart';
+import 'package:provider/provider.dart';
+import 'package:gather_go/shared/loading.dart';
 import 'package:gather_go/services/database.dart';
 
 class CommentScreen extends StatefulWidget {
@@ -17,55 +23,219 @@ class CommentScreen extends StatefulWidget {
 class _CommentScreenState extends State<CommentScreen> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<NewUser?>(context, listen: false);
+    Stream<QuerySnapshot<Map<String, dynamic>>> stream1 = FirebaseFirestore
+        .instance
+        .collection('events/0opY97ALXRrj7UOg482R/messages')
+        .snapshots();
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 40),
-            ),
-            Row(children: [
-              IconButton(
-                icon: new Icon(Icons.arrow_back_ios),
-                onPressed: () {
-                  Navigator.pop(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => eventDetailsForUesers(
-                                event: widget.event,
-                              )));
-                },
-              ),
-              Flexible(
-                child: Text(widget.event?.get('name') + '   ',
-                    style: TextStyle(
-                        color: Colors.deepOrange,
-                        fontFamily: 'Comfortaa',
-                        fontSize: 18)),
-              ),
-            ]),
-            ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: 10,
-              itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text("this works"),
-              ),
-            ),
-          ],
-        ),
-      ),
+      backgroundColor: Colors.white,
+      body: ListView(children: [
+        Container(
+            height: 200,
+            // width: 400,
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(children: [
+                    IconButton(
+                      icon: new Icon(Icons.arrow_back_ios),
+                      onPressed: () {
+                        Navigator.pop(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => eventDetailsForUesers(
+                                      event: widget.event,
+                                    )));
+                      },
+                    ),
+                    Flexible(
+                      child: Text(widget.event?.get('name') + ' comments  ',
+                          style: TextStyle(
+                              color: Colors.deepOrange,
+                              fontFamily: 'Comfortaa',
+                              fontSize: 18)),
+                    ),
+                  ]),
+                  Padding(
+                      padding: const EdgeInsets.all(30),
+                      // padding: const EdgeInsets.only(left: 30),
+                      child: Column(children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(height: 350),
+                              StreamBuilder(
+                                  stream: FirebaseFirestore.instance
+                                      .collection(
+                                          'events/0opY97ALXRrj7UOg482R/messages')
+                                      .snapshots(),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<dynamic> snapshot) {
+                                    if (!snapshot.hasData) {
+                                      return Center(
+                                        child: Loading(),
+                                      );
+                                    }
+
+                                    return Container(
+                                        height: 350,
+                                        width: 280,
+                                        child: ListView(
+                                          children: snapshot.data.docs
+                                              .map<Widget>((document) {
+                                            DocumentSnapshot uid = document;
+                                            return Padding(
+                                                padding: const EdgeInsets.all(
+                                                    8),
+                                                //  const EdgeInsets.only(right: 70),
+                                                child: Card(
+                                                    elevation: 6,
+                                                    shape:
+                                                        RoundedRectangleBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        10),
+                                                            side: BorderSide(
+                                                                width: 0.5,
+                                                                color: Colors
+                                                                    .amber)),
+                                                    margin: const EdgeInsets
+                                                        .fromLTRB(10, 0, 10, 0),
+                                                    //color: Colors.orangeAccent,
+                                                    child: ListTile(
+                                                      title: Center(
+                                                          child: Text(
+                                                        document['text'],
+                                                        textAlign:
+                                                            TextAlign.center,
+                                                        style: TextStyle(
+                                                            color: Colors
+                                                                .amber[600],
+                                                            fontFamily:
+                                                                'Comfortaa',
+                                                            fontSize: 16,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold),
+                                                      )),
+                                                      /*  subtitle: Text(
+                                                  document['date'].toString(),
+                                                  style: TextStyle(
+                                                      color: Colors.amber[600],
+                                                      fontFamily: 'Comfortaa',
+                                                      fontSize: 14),
+                                                ), */
+                                                      // 00:000
+                                                      trailing: Icon(
+                                                        Icons
+                                                            .arrow_forward_ios_sharp,
+                                                        color:
+                                                            Colors.purple[300],
+                                                      ),
+                                                      onTap: () {
+                                                        // Navigator.push(
+                                                        //     context,
+                                                        //     MaterialPageRoute(
+                                                        //         builder: (context) =>
+                                                        //             eventDetailsForUesers(
+                                                        //               event: uid,
+                                                        //               // change to move to details and booked
+                                                        //             )));
+                                                      },
+                                                    )));
+                                          }).toList(), //docmnt
+                                        ));
+                                  })
+                            ])
+                      ]))
+                ]))
+      ]),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
             FirebaseFirestore.instance
-                .collection('events/0opY97ALXRrj7UOg482R /messages')
-                .snapshots()
-                .listen((data) {
-              print(data);
-            });
+                .collection('events/0opY97ALXRrj7UOg482R/messages')
+                .add({'text': 'This was added!'});
+            // FirebaseFirestore.instance
+            //     .collection('events/0opY97ALXRrj7UOg482R/messages')
+            //     .snapshots()
+            //     .listen((data) {
+            //   data.docs.forEach((document) {
+            //     print(document['text']
+            // );
+            // }
+            // );
+            // }
+            // )
           }),
     );
+
+    // return Scaffold(
+    //   body: StreamBuilder(
+    //     stream: FirebaseFirestore.instance
+    //         .collection('events/0opY97ALXRrj7UOg482R/messages')
+    //         .snapshots(),
+    //     builder: (ctx, snapshot) {
+    //       SingleChildScrollView(
+    //         child: Column(
+    //           children: [
+    //             Padding(
+    //               padding: const EdgeInsets.only(top: 40),
+    //             ),
+    //             Row(children: [
+    //               IconButton(
+    //                 icon: new Icon(Icons.arrow_back_ios),
+    //                 onPressed: () {
+    //                   Navigator.pop(
+    //                       context,
+    //                       MaterialPageRoute(
+    //                           builder: (context) => eventDetailsForUesers(
+    //                                 event: widget.event,
+    //                               )));
+    //                 },
+    //               ),
+    //               Flexible(
+    //                 child: Text(widget.event?.get('name') + '   ',
+    //                     style: TextStyle(
+    //                         color: Colors.deepOrange,
+    //                         fontFamily: 'Comfortaa',
+    //                         fontSize: 18)),
+    //               ),
+    //             ]),
+    //             ListView.builder(
+    //               scrollDirection: Axis.vertical,
+    //               shrinkWrap: true,
+    //               itemCount: 10,
+    //               itemBuilder: (ctx, index) => Container(
+    //                 padding: EdgeInsets.all(8),
+    //                 child: Text("this works"),
+    //               ),
+    //             ),
+    //           ],
+    //         ),
+    //       );
+    //     },
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //       child: Icon(Icons.add),
+    //       onPressed: () {
+    //         // FirebaseFirestore.instance
+    //         //     .collection('events/0opY97ALXRrj7UOg482R/messages')
+    //         //     .snapshots()
+    //         //     .listen((data) {
+    //         //   data.docs.forEach((document) {
+    //         //     print(document['text']
+    //         // );
+    //         // }
+    //         // );
+    //         // }
+    //         // )
+    //       }),
+    // );
   }
 }
