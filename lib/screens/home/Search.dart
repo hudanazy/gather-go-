d351dc90909948cd1f2fc9013b89d11f9b7fcf56
shahadAD1 <shahadabdullahd@gmail.com>
2021-11-13 22:08:59 +0300

    change search value

--- lib/screens/home/Search.dart
+++ lib/screens/home/Search.dart
@@ -5,7 +5,7 @@ import 'package:gather_go/shared/loading.dart';
 import 'eventDetailsForUsers.dart';
 
 class search extends StatefulWidget {
-  const search({ Key? key }) : super(key: key);
+  const search({Key? key}) : super(key: key);
 
   @override
   _searchState createState() => _searchState();
@@ -14,75 +14,70 @@ class search extends StatefulWidget {
 class _searchState extends State<search> {
   @override
   Widget build(BuildContext context) {
-    Stream<QuerySnapshot<Map<String, dynamic>>> snap = FirebaseFirestore.instance.collection('events')
-    .where('searchDescription', arrayContains: 'it').snapshots();
-    
+    Stream<QuerySnapshot<Map<String, dynamic>>> snap = FirebaseFirestore
+        .instance
+        .collection('events')
+        .where('searchDescription', arrayContains: 'test')
+        .snapshots();
+
     return StreamBuilder<Object>(
-      stream: snap,
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot){
-        if (snapshot.data.size == 0){
-        return Center(
-          child: Text('No events'),
-        );}
-        return Scaffold(
-          body: ListView(
-            children: snapshot.data.docs
-                                        .map<Widget>((document) {
-                                      DocumentSnapshot uid = document;
-                                      return Padding(
-                                          padding: const EdgeInsets.all(8),
-                                          //  const EdgeInsets.only(right: 70),
-                                          child: Card(
-                                              elevation: 6,
-                                              shape: RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(10),
-                                                  side: BorderSide(
-                                                      width: 0.5,
-                                                      color: Colors.amber)),
-                                              margin: const EdgeInsets.fromLTRB(
-                                                  10, 0, 10, 0),
-                                              //color: Colors.orangeAccent,
-                                              child: ListTile(
-                                                title: Center(
-                                                    child: Text(
-                                                  document['name'],
-                                                  textAlign: TextAlign.center,
-                                                  style: TextStyle(
-                                                      color: Colors.amber[600],
-                                                      fontFamily: 'Comfortaa',
-                                                      fontSize: 16,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                )),
-                                                /*  subtitle: Text(
+        stream: snap,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.data.size == 0) {
+            return Center(
+              child: Text('No events'),
+            );
+          }
+          return Scaffold(
+            body: ListView(
+              children: snapshot.data.docs.map<Widget>((document) {
+                DocumentSnapshot uid = document;
+                return Padding(
+                    padding: const EdgeInsets.all(30),
+                    //  const EdgeInsets.only(right: 70),
+                    child: Card(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(width: 0.5, color: Colors.amber)),
+                        margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        //color: Colors.orangeAccent,
+                        child: ListTile(
+                          title: Center(
+                              child: Text(
+                            document['name'],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.amber[600],
+                                fontFamily: 'Comfortaa',
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          )),
+                          /*  subtitle: Text(
                                                   document['date'].toString(),
                                                   style: TextStyle(
                                                       color: Colors.amber[600],
                                                       fontFamily: 'Comfortaa',
                                                       fontSize: 14),
                                                 ), */
-                                                // 00:000
-                                                trailing: Icon(
-                                                  Icons.arrow_forward_ios_sharp,
-                                                  color: Colors.purple[300],
-                                                ),
-                                                onTap: () {
-                                                  Navigator.push(
-                                                      context,
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              eventDetailsForUesers(
-                                                                event: uid,
-                                                                // change to move to details and booked
-                                                              )));
-                                                },
-                                              )));
-                                    }).toList(), //docmnt
-          ),
-        );
-      }
-    );
-    
+                          // 00:000
+                          trailing: Icon(
+                            Icons.arrow_forward_ios_sharp,
+                            color: Colors.purple[300],
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => eventDetailsForUesers(
+                                          event: uid,
+                                          // change to move to details and booked
+                                        )));
+                          },
+                        )));
+              }).toList(), //docmnt
+            ),
+          );
+        });
   }
 }
