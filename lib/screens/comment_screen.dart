344e4994b9cea6b4fc73cfa9ee4ea0613bc74f8d
hudanazy <90036532+hudanazy@users.noreply.github.com>
2021-11-18 08:56:31 +0300

    fix build error and comment ui- still need scroll and image

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -25,6 +25,7 @@ class CommentScreen extends StatefulWidget {
 class _CommentScreenState extends State<CommentScreen> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     final user = Provider.of<NewUser?>(context, listen: false);
     Stream<QuerySnapshot<Map<String, dynamic>>> snapshot = FirebaseFirestore
         .instance
@@ -36,134 +37,187 @@ class _CommentScreenState extends State<CommentScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: ListView(
+      body: Column(
         children: [
           Container(
             //height: 200,
             // width: 400,
             // padding: EdgeInsets.all(20),
             color: Colors.white,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(children: [
-                    IconButton(
-                      icon: new Icon(Icons.arrow_back_ios),
-                      onPressed: () {
-                        Navigator.pop(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => eventDetailsForUesers(
-                                      event: widget.event,
-                                    )));
-                      },
-                    ),
-                    Flexible(
-                      child: Text(widget.event?.get('name') + ' comments  ',
-                          style: TextStyle(
-                              color: Colors.deepOrange,
-                              fontFamily: 'Comfortaa',
-                              fontSize: 18)),
-                    ),
-                  ]),
-                  // Padding(
-                  //     padding: const EdgeInsets.all(30),
-                  // padding: const EdgeInsets.only(left: 30),
-                  // child:
-                  Column(
-                    children: [
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(height: 350),
-                            StreamBuilder(
-                                stream: snapshot,
-                                builder: (BuildContext context,
-                                    AsyncSnapshot<dynamic> snapshot) {
-                                  if (!snapshot.hasData) {
-                                    return Center(
-                                      child: Text("No comments yet."),
-                                    );
-                                  }
 
-                                  return Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(children: [
+                  IconButton(
+                    icon: new Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      Navigator.pop(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => eventDetailsForUesers(
+                                    event: widget.event,
+                                  )));
+                    },
+                  ),
+                  Flexible(
+                    child: Text(widget.event?.get('name') + ' comments  ',
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontFamily: 'Comfortaa',
+                            fontSize: 18)),
+                  ),
+                ]),
+                // Padding(
+                //     padding: const EdgeInsets.all(30),
+                // padding: const EdgeInsets.only(left: 30),
+                // child:
+                Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(height: 350),
+                          StreamBuilder(
+                              stream: snapshot,
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<dynamic> snapshot) {
+                                if (!snapshot.hasData) {
+                                  return Center(
+                                    child: Text("No comments yet."),
+                                  );
+                                }
+
+                                return Padding(
+                                  padding: EdgeInsets.only(top: 10, right: 20),
+                                  child: Container(
                                     height: 350,
                                     width: 280,
                                     child: Expanded(
-                                      child: ListView(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
                                         //reverse: true,
                                         children: snapshot.data.docs
                                             .map<Widget>((document) {
                                           DocumentSnapshot uid = document;
-                                          return Padding(
-                                              padding: const EdgeInsets.all(8),
-                                              //  const EdgeInsets.only(right: 70),
-                                              child: Card(
-                                                  elevation: 6,
-                                                  shape: RoundedRectangleBorder(
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              10),
-                                                      side: BorderSide(
-                                                          width: 0.5,
-                                                          color: Colors.amber)),
-                                                  margin:
-                                                      const EdgeInsets.fromLTRB(
-                                                          10, 0, 10, 0),
-                                                  //color: Colors.orangeAccent,
-                                                  child: ListTile(
-                                                    title: Center(
-                                                        child: Text(
-                                                      document['text'],
-                                                      textAlign:
-                                                          TextAlign.center,
-                                                      style: TextStyle(
-                                                          color:
-                                                              Colors.amber[600],
-                                                          fontFamily:
-                                                              'Comfortaa',
-                                                          fontSize: 16,
-                                                          fontWeight:
-                                                              FontWeight.bold),
-                                                    )),
-                                                    /*  subtitle: Text(
-                                                        document['date'].toString(),
-                                                        style: TextStyle(
-                                                            color: Colors.amber[600],
-                                                            fontFamily: 'Comfortaa',
-                                                            fontSize: 14),
-                                                      ), */
-                                                    // 00:000
-                                                    trailing: Icon(
-                                                      Icons
-                                                          .arrow_forward_ios_sharp,
-                                                      color: Colors.purple[300],
+                                          return Card(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10)),
+                                            child: Container(
+                                              padding: EdgeInsets.only(
+                                                  top: 10, left: 10, right: 10),
+                                              decoration: BoxDecoration(
+                                                  color: Colors.grey
+                                                      .withOpacity(.3),
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10)),
+                                              child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.end,
+                                                  // padding: const EdgeInsets.all(8),
+                                                  children: [
+                                                    Container(
+                                                      width: width / 1.5,
+                                                      child: Text(
+                                                          document['text']),
                                                     ),
-                                                    onTap: () {
-                                                      // Navigator.push(
-                                                      //     context,
-                                                      //     MaterialPageRoute(
-                                                      //         builder: (context) =>
-                                                      //             eventDetailsForUesers(
-                                                      //               event: uid,
-                                                      //               // change to move to details and booked
-                                                      //             )));
-                                                    },
-                                                  )));
+                                                    Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment.end,
+                                                      children: [
+                                                        IconButton(
+                                                          onPressed:
+                                                              () async {},
+                                                          icon: Icon(Icons
+                                                              .thumb_up_alt_rounded),
+                                                          color: Colors.grey,
+                                                          iconSize: 20,
+                                                        ),
+                                                        Text('0'),
+                                                        IconButton(
+                                                          onPressed:
+                                                              () async {},
+                                                          icon: Icon(Icons
+                                                              .thumb_down_alt_rounded),
+                                                          color: Colors.grey,
+                                                          iconSize: 20,
+                                                        ),
+                                                        Text('0'),
+                                                      ],
+                                                      //  const EdgeInsets.only(right: 70),
+                                                      // child: Card(
+                                                      //     elevation: 6,
+                                                      //     shape: RoundedRectangleBorder(
+                                                      //         borderRadius:
+                                                      //             BorderRadius.circular(
+                                                      //                 10),
+                                                      //         side: BorderSide(
+                                                      //             width: 0.5,
+                                                      //             color: Colors.amber)),
+                                                      //     margin:
+                                                      //         const EdgeInsets.fromLTRB(
+                                                      //             10, 0, 10, 0),
+                                                      //     //color: Colors.orangeAccent,
+                                                      //     child: ListTile(
+                                                      //       title: Center(
+                                                      //           child: Text(
+                                                      //         document['text'],
+                                                      //         textAlign:
+                                                      //             TextAlign.center,
+                                                      //         style: TextStyle(
+                                                      //             color:
+                                                      //                 Colors.amber[600],
+                                                      //             fontFamily:
+                                                      //                 'Comfortaa',
+                                                      //             fontSize: 16,
+                                                      //             fontWeight:
+                                                      //                 FontWeight.bold),
+                                                      //       )),
+                                                      //       /*  subtitle: Text(
+                                                      //           document['date'].toString(),
+                                                      //           style: TextStyle(
+                                                      //               color: Colors.amber[600],
+                                                      //               fontFamily: 'Comfortaa',
+                                                      //               fontSize: 14),
+                                                      //         ), */
+                                                      //       // 00:000
+                                                      //       trailing: Icon(
+                                                      //         Icons
+                                                      //             .arrow_forward_ios_sharp,
+                                                      //         color: Colors.purple[300],
+                                                      //       ),
+                                                      //       onTap: () {
+                                                      //         // Navigator.push(
+                                                      //         //     context,
+                                                      //         //     MaterialPageRoute(
+                                                      //         //         builder: (context) =>
+                                                      //         //             eventDetailsForUesers(
+                                                      //         //               event: uid,
+                                                      //         //               // change to move to details and booked
+                                                      //         //             ))
+                                                    ),
+                                                  ]),
+                                            ),
+                                          );
+                                          //       },
+                                          //     )));
                                         }).toList(), //docmnt
                                       ),
                                     ),
-                                  );
-                                })
-                          ]),
-                    ],
-                  )
-                  // )
-                ],
-              ),
+                                  ),
+                                );
+                              })
+                        ]),
+                  ],
+                )
+                // )
+              ],
             ),
           ),
+
           // Expanded(
           //   child: Align(
           //     alignment: FractionalOffset.bottomCenter,
